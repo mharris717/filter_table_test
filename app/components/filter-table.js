06d2d10cfcd5ad5f10db6b1ded70d3253968a4f4
Mike Harris <mharris717@gmail.com>
2014-10-15 11:34:34 -0400

    also works

--- app/components/filter-table.js
+++ app/components/filter-table.js
@@ -6,19 +6,24 @@ export default Filtertable.extend({
     console.debug("in my afterLoadRecords");
   }.observes("loadRecordsZZZ"),
 
-  loadRecords: function() {
-    console.debug("my loadRecords start");
-    this._super();
+  // loadRecords: function() {
+  //   console.debug("my loadRecords start");
+  //   this._super();
 
-    var filtered = this.get('filteredRecords');
-    // if (filtered.length > 99) {
-    //   filtered = Em.A([filtered[0]]);
-    //   this.set("filteredRecords",filtered);
-    // }
+  //   var filtered = this.get('filteredRecords');
+  //   // if (filtered.length > 99) {
+  //   //   filtered = Em.A([filtered[0]]);
+  //   //   this.set("filteredRecords",filtered);
+  //   // }
 
-    filtered = PagedArray.create({content: filtered, perPage: 5});
-    this.set("filteredRecords",filtered);
+  //   filtered = PagedArray.create({content: filtered, perPage: 5});
+  //   this.set("filteredRecords",filtered);
 
-    console.debug("my loadRecords end");
-  }.observes('textFilter', 'reloadRecords')
+  //   console.debug("my loadRecords end");
+  // }.observes('textFilter', 'reloadRecords'),
+
+  pagedRecords: function() {
+    var filtered = this.get("filteredRecords");
+    return PagedArray.create({content: filtered, perPage: 5});
+  }.property("filteredRecords.@each")
 });
