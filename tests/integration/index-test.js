ab8f64dd4dbc72cc831f92f2b472640c910b7ac0
Mike Harris <mharris717@gmail.com>
2014-10-15 11:31:56 -0400

    quick pagination tests

--- tests/integration/index-test.js
+++ tests/integration/index-test.js
@@ -1,6 +1,7 @@
 import Em from 'ember';
 import startApp from '../helpers/start-app';
 import { test } from 'ember-qunit';
+import PaginationAssertions from '../helpers/assertions';
 
 var App;
 
@@ -17,6 +18,7 @@ test("smoke", function() {
   visit("/posts").then(function() {
     equal(find("table").length,1);
     equal(find("td.author").length,5);
+    equal(find("td.title:eq(0)").text(),"Title 1");
   });
 });
 
@@ -26,5 +28,32 @@ test("basic filter", function() {
 
   andThen(function() {
     equal(find("td.author").length,4);
+  });
+});
+
+test("pagination controls", function() {
+  visit("/posts");
+
+  andThen(function() {
+    equal(find(".pagination .page-number").length,2);
+  });
+});
+
+test("pagination controls after filter", function() {
+  visit("/posts");
+  fillIn("input[type=text]","Adam");
+
+  andThen(function() {
+    equal(find(".pagination .page-number").length,1);
+  });
+});
+
+test("pagination click", function() {
+  visit("/posts");
+  clickPage(2);
+
+  andThen(function() {
+    hasActivePage(2);
+    equal(find("td.title:eq(0)").text(),"Title 6");
   });
 });
